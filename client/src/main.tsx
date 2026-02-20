0a86b6ad48b025f0ea7a0d5c7e9977df23e0b02a
Chen Ye <cye@alumni.brown.edu>
2026-02-20 03:23:51 -0800

    fix service work

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,7 +7,7 @@ import App from './App.tsx'
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').then((registration) => {
+    navigator.serviceWorker.register(`${import.meta.env.BASE_URL}sw.js`).then((registration) => {
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, (err) => {
       console.log('ServiceWorker registration failed: ', err);
